0c97cb1a7c1fac65369edfdb16a74cdc0daff3c8
Olof Enström <olofens@student.chalmers.net>
2018-07-13 17:29:49 +0200

    initial commit

---
+++ planify/planify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  planify
+//
+//  Created by Olof Enström on 2018-07-13.
+//  Copyright © 2018 Olof Enström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
